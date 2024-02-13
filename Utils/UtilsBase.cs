a9ada87be55ed87aef5e076e3f77bcc83cd9720e
Mikael Italo <mikael.italo.35@hotmail.com>
2024-02-13 16:22:10 -0300

    Create base requests Users and Loan region

--- Utils/UtilsBase.cs
+++ Utils/UtilsBase.cs
@@ -5,11 +5,14 @@
         public static RestClient Client = new (GetValueJsonGlobalVariables("Url"));
         public static RestResponse? Response { get; set; }
         public UtilsBase() {  }
-        
+
         /// <summary>
         /// RestRequest Methods for implementation at steps.cs
         /// Métodos RestRequest para implementação em steps.cs 
         /// </summary>
+
+        #region Book Requests
+
         public static RestRequest BookAvailibleGet()
         {
             RestRequest request = new ($"book/availible", Method.Get);
@@ -67,6 +70,76 @@
             return request;
         }
 
+        #endregion Book Requests
+
+        #region BookLoan Requests
+
+
+
+        #endregion BookLoan Requests
+
+        #region Users Requests
+
+        public static RestRequest UserGet()
+        {
+            RestRequest request = new($"user", Method.Get);
+
+            return request;
+        }
+
+        public static RestRequest UserPost(string Name, string Email, string PhoneNumber)
+        {
+            RestRequest request = new($"user", Method.Post);
+            request.AddJsonBody(new
+            {
+                name = Name,
+                email = Email,
+                phoneNumber = PhoneNumber,
+            });
+
+            return request;
+        }
+        
+        public static RestRequest UserPut(string Id, string Name, string Email, string PhoneNumber)
+        {
+            RestRequest request = new($"user", Method.Put);
+            request.AddJsonBody(new
+            {
+                id = Id,
+                name = Name,
+                email = Email,
+                phoneNumber = PhoneNumber,
+            });
+
+            return request;
+        }
+
+        public static RestRequest UserGetAvailibles()
+        {
+            RestRequest request = new($"user/availibles", Method.Get);
+
+            return request;
+        }
+
+        public static RestRequest UserIdGet(string Id)
+        {
+            RestRequest request = new("user/{id}", Method.Get);
+            request.AddParameter("id", Id);
+
+            return request;
+        }
+
+        public static RestRequest UserIdDelete(string Id)
+        {
+            RestRequest request = new("user/{id}", Method.Delete);
+            request.AddParameter("id", Id);
+
+            return request;
+        }
+
+        #endregion Users Requests
+
+        #region Métodos úteis
         public static string GetValueJsonGlobalVariables(string key)
         {
             StreamReader r = new("GlobalVariables.json");
@@ -76,5 +149,6 @@
 
             return Value;
         }
+        #endregion Métodos úteis
     }
 }
